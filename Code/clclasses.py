fead0e3978559eab8c96cfca1bfd4f0b4c936012
Johnny Klonaris <coLab-dev@jawknee.com>
2012-12-09 23:19:56 -0800

    Major change - classifiction of groups/pages
    Moved most functionality into the Groups and Pages classes.
    Also, one more bit reorg:  from /coLab we have Groups, within
    groups are Pages and eventually Projects and Songs.

---
+++ Code/clclasses.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python
+"""
+	classes for defining the various data file fomrats
+
+"""
+
+import os
+import imp
+import cldate
+import clutils
+
+class Group:
+	"""
+	Data associated with a group
+	"""
+	def __init__(self):
+		self.name = "<unset>"
+		self.title = "<unset>"
+		self.subtitle = "<unset>"
+		self.collaborators = "<unset>"
+		self.description = "<unset>"
+		# arbitrary date just after the epoch
+		self.create=cldate.string2utc("1970-01-24T09:23:45")
+		self.update=cldate.string2utc("1970-01-24T09:23:45")
+		
+		self.pagelist = []
+
+
+
+	def load(self,name):	
+		"""
+		Load the group data - starting at the local coLab root, 
+		and also load all the related data, e.g., pages.
+		"""
+		try:
+			name
+		except NameError, info:
+			print "Group load - no name passed.", info
+			raise NameError
+
+		self.name = name
+
+		# load in the site data to get started...
+		# coLab_url_head	- head to the ext web site (e.g., http://...)
+		# coLab_root		- url to the local structure (e.g., /coLab )
+		# coLab_home		- local fs location of coLab home
+		#
+		try:
+			conf=clutils.get_config()
+		except ImportError:
+			print "Cannot find config."
+			sys.exit(1)
+
+		# populate the group paths/locations
+		# first - the site wide values	 - no need to config more than once..
+		self.coLab_url_head = conf.coLab_url_head
+		self.coLab_root = conf.coLab_root
+		self.coLab_home = conf.coLab_home
+		# and the group specific locations
+		self.url_head = os.path.join(conf.coLab_url_head, 'Group', name)
+		self.root = os.path.join(conf.coLab_root, 'Group', name)
+		self.home = os.path.join(conf.coLab_home, 'Group', name)
+
+		#
+		# Load a generic object from the file, then transfer the items 
+		# to the current Group object
+
+		try:
+			data = os.path.join(self.home, 'data')
+			d = imp.load_source('', data)
+			os.remove(data + 'c')	# compiled...
+		except (ImportError, IOError), info:
+			print "Group,load - cannot access:", data
+			print info
+			raise ImportError
+		
+
+		# populate the group data from the imported data file...
+		try:
+			self.title = d.title
+			self.subtitle = d.subtitle
+			self.collaborators = d.collaborators
+			self.description = d.description
+
+		except AttributeError, info:
+			print "group.load - import error", info
+			raise ImportError
+			
+		# Now, step into the pages dir and load up a list of pages
+		pagedir = os.path.join(self.home, 'Page')
+
+		try:
+			os.chdir(pagedir)
+		except:
+			print "Cannot get to dir:", pagedir
+			print "Fatal"
+			raise ImportError()
+			
+		# Now step through the dirs  (and files - rejecting them...).
+		
+		for dir in (os.listdir('.')):
+			#print "dir is:", dir
+
+			page = Page() # instance
+	
+			try:
+				page.load(dir)
+			except IOError:
+				#print "problem with", dir
+				continue
+	
+			page.url_head = os.path.join(self.url_head, 'Page', page.name)
+			page.root = os.path.join(self.root, 'Page', page.name)
+			page.home = os.path.join(self.home, 'Page', page.name)
+
+			self.pagelist.append(page)
+
+
+
+
+
+
+# 
+# For sorting - return the appropriate time in seconds
+def updatekey(self):
+	return (self.update)	# return the update time as seconds for sorting
+def createkey(self):
+	return (self.create)	# ditto the create time
+class Page:
+	"""
+	The data associated with a song, imported from 
+	a raw file - at this point.
+	"""
+	
+	def __init__(self):
+		self.name="<unset>"
+		self.desc_title="<unset>"
+		self.fun_title="<unset>"
+		self.description="<unset>"
+
+		self.project="<unset>"
+		self.assoc_projects=''
+
+		self.prevlink="<unset>"
+		self.nextlink="<unset>"
+
+		# initial value close to the epoch
+		self.create=cldate.string2utc("1970-01-24T09:23:45")
+		self.update=cldate.string2utc("1970-01-24T09:23:45")
+
+	def load(self,path):
+		"""
+		This is a bit kludgy right now, 
+		we read the file, then populate
+		the song object with the data.
+
+		We could just create a list of the imported
+		objects, but we'd have no idea what 
+		vars were involved and no methods.   Pickling
+		could work - if we want to commit to Python.
+
+		For now, this is generic and flexible
+		"""
+
+		#
+		# if path is passed in, use it - otherwise
+		# look in the current dir...
+		try:
+			path
+		except:
+			path = '.'
+
+		dfile = os.path.join(path, 'data')
+		#print "dfile:",dfile
+
+		#
+		try:
+			P = imp.load_source('',dfile)
+			os.remove(dfile + 'c')
+		except IOError, info:
+			#print "Immport problem with", dfile, info
+			raise IOError
+		#
+		# Step through them...
+		try:
+			self.name = P.name
+			self.desc_title = P.desc_title
+			self.fun_title = P.fun_title
+			self.description = P.description
+
+			self.project = P.project
+			self.assoc_projects = P.assoc_projects
+
+			self.prevlink = P.prevlink
+			self.nextlink = P.nextlink
+
+			self.create = cldate.string2utc(P.create)
+			self.update = cldate.string2utc(P.update)
+
+		except (NameError, AttributeError), info:
+			print "Note: unset var in data file", info
+
+
+
+	def dump(self):
+		"""
+		Output in a format suitable for replacing the data file
+		"""
+		EOL = '"\n'
+
+		return( 'name="' + self.name + EOL +
+			'desc_title="' + self.desc_title + EOL +
+			'fun_title="' + self.fun_title + EOL +
+			'\n' +
+			'project="' + self.project + EOL +
+			'assoc_projects="' + self.assoc_projects + EOL +
+			'description="""' + self.description +
+			'"""\n' +
+			'\n' +
+			'prevlink="' + self.prevlink + EOL +
+			'nextlink="' + self.nextlink + EOL +
+			'\n' +
+			'create="' + cldate.utc2string(self.create) + EOL +
+			'update="' + cldate.utc2string(self.update) + EOL )
+
+
+
+def main():
+	print "Welcome to clases..."
+	
+	
+	g = Group()
+	g.load('Catharsis')
+
+	for p in g.pagelist:
+		print "got page:", p.name, p.desc_title, p.fun_title
+
+
+if __name__ == '__main__':
+	main()
--- Code/htmlgen.py
+++ Code/htmlgen.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 """
 	Create a Web dir based on the name passed.
 
@@ -14,50 +13,35 @@
 
 import os
 import sys
-import datetime
 import imp	# to input the data file
 import shutil
-import coLabUtils
 
-def htmlgen(name):
+import clutils
+import cldate
+from clclasses  import *
 
+def htmlgen(group, page):
+	"""
+		Passed the name of the group and page, rebuilds
+		the index.shtml 
+	"""
 
+	# these go a way...
+	#domain_root = conf.coLab_home + "/Domains/Catharsis"
+	#piece = domain_root + "/Pieces/" + name
 
 	try:
-		conf=coLabUtils.get_config()
-	except ImportError:
-		print "Cannot find config."
-		sys.exit(1)
-
-	domain_root = conf.coLab_home + "/Domains/Catharsis"
-	piece = domain_root + "/Pieces/" + name
-
-	try:
-		os.chdir(piece)
+		os.chdir(page.home)
 	except OSError,info:
-		print "Cannot cd to ", piece, info
+		print "Cannot cd to ", page.home
 		print "fatal."
 		sys.exit(1)
 
 
-	date = str(datetime.datetime.now())
-
-	if os.path.exists('data'):
-		print "Found data file"
-	else:
-		src=conf.coLab_home + "/Resources/domain_home_data.template"
-		try:
-			shutil.copy(src, 'data')
-			f=open('data','a')
-			dates="""\ncreate=\"""" + date + """\"\nupdate=\"""" + date + """\"\n"""
-			f.write(dates)
-			f.close
-		except OSError, info:
-			print "Failed to create data file", info
-			sys.exit(1)
-		print "Created new data file"
+	now = cldate.now()
 
 	index='index.shtml'
+	# for now - shuffle the current one to a temp location...
 	if os.path.exists(index):
 		try:
 			shutil.move(index, index + ".prev")
@@ -66,18 +50,6 @@ def htmlgen(name):
 			sys.exit(1)
 
 
-	try:
-		P = imp.load_source('','data')
-
-	except: 
-		print "Problem importing the data... ", sys.exc_info()[0]
-		print "Cannot continue."
-		os.system("/bin/pwd")
-		os.system("cat data")
-		exit(1)
-
-
-	os.system("rm -f datac c")	# consequece of the import...
 
 	try:
 		for file in ("Comments.log", "links.html" ):
@@ -85,10 +57,6 @@ def htmlgen(name):
 			f.close()
 	except IOError, info:
 		print "Touch problem", file, info
-
-
-
-	date = str(datetime.datetime.now())
 
 	try:
 		outfile = open(index, 'w+')
@@ -133,9 +101,11 @@ def htmlgen(name):
 		</object>
 		</noscript>
 	"""
+	# Update the strings....
+	media_insert.replace("<name>", page.name)
 
 	head = """<html>
-		<head><title>""" + P.fun_title + """</title>
+		<head><title>""" + page.fun_title + """</title>
 		<link rel="stylesheet" type="text/css" href="/coLab/Resources/Style_Default.css">
 		<link rel="shortcut icon" href="/coLab/Resources/CoLab_Logo.png">
 		""" + head_insert + "</head>"
@@ -149,33 +119,35 @@ def htmlgen(name):
 		<div class="banner" > <! start of banner>
 		<!center>	 
 		        <table width=80% border=0 cellpadding=0 class="banner_txt">
-		          <td align="center" ><a href="!coLabRoot!/index.shtml" title="Always a nice place to go...">Home</a></td>
-		          <td align="center" ><a href="!coLabRoot!/Shared/new.shtml" title="The place to be, if you want to be somewhere else.">What's&nbsp;New</a></td>
-		          <td align="center" ><a href="!coLabRoot!/Shared/nav.shtml" title="How to get to where you need go.">Nav</a></td>
+		          <td align="center" ><a href="!groupURL!/index.shtml" title="Always a nice place to go...">Home</a></td>
+
+		          <td align="center" ><a href="!groupURL!/Shared/new.shtml" title="The place to be, if you want to be somewhere else.">What's&nbsp;New</a></td>
+		          <td align="center" ><a href="!groupURL!/Shared/nav.shtml" title="How to get to where you need go.">Nav</a></td>
 		          <td align="center" ><a href="!coLabRoot!/Help/" title="Hopefully, the help you need.">Help</a></td>
 		        </tr></table>
 		<!/center>
 		<br>
 		</div>	<! end of banner>
 
-		<!--#include virtual="/coLab/Shared/oldsidebar_l.html" -->
+		<!--#include virtual="/coLab/Shared/mostrecent.html" -->
 		<div id="Logo" class="logo"><img src="/coLab/Resources/CoLab_Logo.png" height=50 width=50></div>
 
 		
 		<div id="Content" class="main">
 		<center>
 		<h1 class=fundesc>
-	""" + P.fun_title + "</h1>"
+	""" + page.fun_title + "</h1>"
 
-	body = body.replace('!coLabRoot!', conf.coLab_root)
+	body = body.replace('!groupURL!', group.root)
+	body = body.replace('!coLabRoot!', group.coLab_root)
 
 	content = """
 		</center>
 
 	<!--#include virtual="links.html" -->
 	<div class="maintext">
-	<h2 class=fundesc>""" + P.desc_title + """</h2>
-	<font color=a0b0c0>""" + P.description + "<p><i>" + P.create + """</i><p>
+	<h2 class=fundesc>""" + page.desc_title + """</h2>
+	<font color=a0b0c0>""" + page.description + "<p><i>" + cldate.utc2long(page.create) + """</i><p>
 	<p>"""
 
 	CommentLog="Comments.log" 
@@ -187,8 +159,8 @@ def htmlgen(name):
 	<form method=POST action="/coLab/bin/postcomments.cgi">
 	Your name: <input type="text" name="Commenter" ><br>
 	<center>
-	<input type="hidden" name="site" value=""" + '"' + name + '"' + """>
-	<input type="hidden" name="desc_title" value=""" + '"' + P.desc_title + '"' + """>
+	<input type="hidden" name="page" value=""" + '"' + page.name + '"' + """>
+	<input type="hidden" name="desc_title" value=""" + '"' + page.desc_title + '"' + """>
 	<textarea name="Text" rows=15 cols=80></textarea>
 	<br>
 	</center>
@@ -224,20 +196,91 @@ def htmlgen(name):
 	write(head)
 	write(head_insert)
 	write(body)
-	write(media_insert.replace("<name>", name))
+	write(media_insert.replace("<name>", page.name))
 	write(content)
 	write(tail)
 
 	outfile.close()
 	
-	exit(0)
+	return()
 
+def linkgen(group):
+	# set up for the first and last links - cheat a bit - add a fake
+	# page to the end of the list named "Archive" 
+	#
+	# A bit tricky...   append the list with a fake page: "Archive"
+	p = Page()
+	p.name = "Archive"
+	p.home = os.path.join(group.coLab_root, 'Shared', 'Archive')
+	group.pagelist.append(p)
 
+	# "dummy" current entry for the home...
+	currName = "Home"
+	currTitle = "Home"
+	currFun = group.name + " Home"
+	currLink = group.root
+
+	prevName = "not set yet"
+	# step through the pages in order
+	for p in group.pagelist:
+		print ' '
+		print 'lg:',p.name
+		if p.name == 'Archive':
+			nextName = 'Archive'
+			nextTitle = "Archive"
+			nextFun = "Archive - a place for everything"
+			nextLink = p.home
+		else:
+			nextName = p.name
+			nextTitle = p.desc_title
+			nextFun = p.fun_title
+			nextLink = "../" + p.name	 # yeah, I know, cheating
+
+		print 'middle of loop lg:',p.name, 'prev', prevName, 'curr', currName, 'next', nextName
+		if currName != 'Home':
+			linkfile = os.path.join(currPath, 'links.html')
+			print "Creating linkfile", linkfile
+			try:
+				l = open(linkfile, 'w+')
+			except IOError, info:
+				print "problem opening", linkfile, info
+				raise IOError
+
+			l.write( '<div class="links" style="float: left;">' + 
+				'<a href="' + prevLink + '" title="' + prevFun +
+					'">&larr; ' + prevTitle + '</a></div>' +
+				'<div class="links" style="float: right;">' +
+				'<a href="' + nextLink + '" title="' + nextFun +
+					'">' + nextTitle + ' &rarr;</a></div>' +
+				'<br>' )
+
+				
+			l.close()
+			
+
+		prevName = currName
+		prevTitle = currTitle
+		prevFun = currFun
+		prevLink = currLink
+
+		currPath = p.home
+		currName = nextName
+		currTitle = nextTitle
+		currFun = nextFun
+		currLink = nextLink
+		print 'end of loop lg:',p.name, 'prev', prevName, 'curr', currName, 'next', nextName
+
+	group.pagelist.pop()
+
+				
 if __name__ == "__main__":
+
 	if len(sys.argv) < 2:
-		print "Usage:", sys.argv[0], "<Sitename>"
+		print "Usage:", sys.argv[0], "page name"
 		exit(1)
+
 	name=sys.argv[1]
-	htmlgen(name)
+
+	htmlgen('Catharsis', name)
 
 
